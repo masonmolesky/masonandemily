349c6f2f2a42561c7bc1a956dfc5a7feabd80846
Mason Molesky <masonmolesky@gmail.com>
2024-11-29 20:30:39 -0500

    script update 5

--- signin.js
+++ signin.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // rsvpForm.classList.remove('hidden'); //REMOVE BEFORE GOING LIVE <-------------------------
 
             // Fetch RSVP data from Google Sheets based on the hashValue // AKfycbw6YvcMhO3KnELG4CuA9JxQB1gZ8qBH0QV9HzElLj8
-            fetch(`https://script.google.com/macros/s/AKfycbxT3kowaxi_vphShUcrfZiRsiLtIxCsomw73uCw53Q7CgykC2D2x95DPCgN36yTkajW/exec?hashValue=${hashedValue}`,)
+            fetch(`https://script.google.com/macros/s/AKfycbzFKnmRUuzZ_1urtfU2VukZqSWCzbDxT2IFK-4FJrMmpcFozRLIrwnzCMEg9ZsaQhjp/exec?hashValue=${hashedValue}`, {method: 'GET', mode: 'cors'})
                 .then(response => {
                     if (!response.ok) {
                         throw new Error("Network response was not ok");
